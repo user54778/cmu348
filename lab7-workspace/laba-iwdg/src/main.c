2c01681834e877cb5e3271e22cf0c67423ed6993
Ahron Pollak <ahronrocks51@gmail.com>
2026-04-22 13:41:12 -0400

    finish lab7-part a iwdg
    implemented a timing loop that read from gpio connected to 8 pin DIP
    switch and used value as part of 32-bit int. determined max and min
    value to allow the loop to run indefinitely without walking IWDG.

--- lab7-workspace/laba-iwdg/src/main.c
+++ lab7-workspace/laba-iwdg/src/main.c
@@ -8,6 +8,7 @@
 #include "stm32f0xx_hal.h"
 #include "stm32f0xx_hal_def.h"
 #include "stm32f0xx_hal_gpio.h"
+#include "stm32f0xx_hal_iwdg.h"
 #include "stm32f0xx_hal_rcc.h"
 #include "stm32f0xx_hal_spi.h"
 #include "stm32f0xx_hal_uart.h"
@@ -42,30 +43,46 @@ int main(void) {
   MX_GPIO_Init();
   MX_USART2_UART_Init();
 
-  for (int i = 0; i < 5; i++) {
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-    HAL_Delay(100);
-  }
-
-  // TODO: Configure IWDG with IWDG->PR, IWDG->RLR.
-  // TODO: At startup, check RCC->CSR for IWDGRSTF. If set, illuminate a "reset
-  // indicator" LED and clear the flag, with the purpose of mimicing a reset
-  // vector handler.
-  // TODO: Implement the following timing loop.
   HAL_GPIO_WritePin(GPIOB,
                     GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
                         GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,
                     GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC,
+                    GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
+                        GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,
+                    GPIO_PIN_RESET);
 
-  HAL_GPIO_WritePin(GPIOB,
-                    GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
-                        GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,
-                    GPIO_PIN_SET);
+  // Check if the IWDG was reset
+  if ((__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST))) {
+    for (int i = 0; i < 5; i++) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+      HAL_Delay(100);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+      HAL_Delay(100);
+    }
+    __HAL_RCC_CLEAR_RESET_FLAGS();
+  }
 
+  // Configure IWDG with IWDG->PR, IWDG->RLR.
+  // 1. Enable the IWDG by writing 0x0000 CCCC in the IWDG key register
+  // (IWDG_KR).
+  // 2. Enable register access by writing 0x0000 5555 in the IWDG key register
+  // (IWDG_KR).
+  // 3. Write the prescaler by programming the IWDG prescaler register (IWDG_PR)
+  // from 0 to 7.
+  // 4. Write the IWDG reload register (IWDG_RLR).
+  // 5.Wait for the registers to be updated (IWDG_SR = 0x0000 0000).
+  // 6.Refresh the counter value with IWDG_RLR (IWDG_KR = 0x0000 AAAA).
+  IWDG->KR = IWDG_KEY_ENABLE;              // 1
+  IWDG->KR = IWDG_KEY_WRITE_ACCESS_ENABLE; // 2
+  IWDG->PR = IWDG_PRESCALER_32;            // 3 (given in pre-lab)
+  IWDG->RLR = 0x7CF;                       // 4 (decimal 1999)
+  while (IWDG->SR)
+    ;
+  // should take 1.6 seconds to count down.
+  // TODO: Implement the following timing loop.
   /*
-loopForever:
+loopForever: -> read dip switch value, update LED, kick wdg
     load LOOP_PARAMETER into loop counter register
     ; --- some work here (e.g., update output) ---
     ; POINT 1
@@ -80,19 +97,39 @@ waitLoop:
     ; POINT 1 (next iteration)
     B loopForever
     */
-  // TODO: Read the GPIO input pins at the top of each outer loop iteration to
+  //  Read the GPIO input pins at the top of each outer loop iteration to
   // form LOOP_PARAMETER. Use the read value as the MSB of a 16-bit counter.
-  //  TODO: Use the DWT cycle counter to verify the loop timing matches the
+  //  Use the DWT cycle counter to verify the loop timing matches the
   //  formula we pre-computed on paper.
-  //  TODO: Walk the dog after the delay.
-  //  TODO: Find the minimum and maximum GPIO (switch) values that allow the
+  //   Walk the dog after the delay.
+  //  Find the minimum and maximum GPIO (switch) values that allow the
   //  loop to run indefinitely without a watchdog reset.
-
   while (1) {
+    uint8_t dipSwitchValue =
+        ~(GPIOC->IDR >> 0) & 0xFF; // extract pins 0-7 of PC0-PC7
+    // uint16_t loopParameter = dipSwitchValue << 8;
+    //  Pick a constant s.t. you map 8-bit input into 32-bit loop count
+    //  Assuming switch value of 128...
+    //  Min = 1.
+    //  Max < 64.
+    const uint32_t scale = 20000;
+    uint32_t loopParameter = dipSwitchValue * scale;
+    while (loopParameter > 0) {
+      __asm volatile("NOP");
+      loopParameter--;
+    }
+    // DIP switch gives 8-bit value; shift it left 8 bits to give MSB of
+    // 16-bit inner loop counter. This is so we have a higher value to count
+    // from. This inner loop will then count down from the value, doing NOPs per
+    // iteration after the inner loop finishes, walk the wdg.
+    /*
+    HAL_Delay(10);
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-    HAL_Delay(1000);
+    HAL_Delay(300);
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-    HAL_Delay(1000);
+    HAL_Delay(300);
+    */
+    IWDG->KR = IWDG_REFRESH; // refresh WDG timer
   }
 }
 
@@ -221,3 +258,18 @@ void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
+/*
+if (dipSwitchValue) {
+  HAL_GPIO_WritePin(GPIOB, dipSwitchValue, GPIO_PIN_SET);
+} else {
+  HAL_GPIO_WritePin(GPIOB, dipSwitchValue, GPIO_PIN_RESET);
+}
+*/
+/*
+if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0) == GPIO_PIN_RESET) {
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+} else {
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+}
+*/
